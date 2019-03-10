f09643b88f8668df80763fcdcd8aa016f2596bc1
bensontjohn <bensontjohn@gmail.com>
2019-03-10 17:11:18 +0000

    Added comments to the primes.py file

--- primes.py
+++ primes.py
@@ -1,12 +1,23 @@
+# Benson Thomas John, 2019
+# Program that asks the user to input a positive integer and tells the user whether or not the number is a prime. 
+
+#Prompt user input and convert the user_input to integer
 user_input = int(input("Please enter a positive integer: "))
 
+# Referenced: https://www.geeksforgeeks.org/python-program-to-check-whether-a-number-is-prime-or-not/
+
+
+# If user_input is greater than one
 if user_input > 1:
-    for n in range(2, user_input):
-        if (user_input % n) == 0:
+    # Creating for loop to range throught the values starting 2 to user_input
+    for num in range(2, user_input):
+
+        # Check to see if user_input is dvisible, if so then not a prime number
+        if (user_input % num) == 0:
             print("That is not a prime number")
             break
     else:
-        # loop fell through without finding a factor
+            # loop fell through without finding a factor
             print("That is a prime number")
 
 else: 
