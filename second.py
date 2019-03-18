767fda42d1103d24efdd7245ec6ffd16002d8a73
bensontjohn <bensontjohn@gmail.com>
2019-03-18 13:18:37 +0000

    Updated code to accept filename from command line

--- second.py
+++ second.py
@@ -1,8 +1,10 @@
-contents = open("File.txt", "r")
+import sys
+
+contents = open(sys.argv[1], "r")
 count = 0
 for line in contents:
     count = count + 1
     if count % 2 == 0:
-        print(line)
+        print(line, end='')
 
 contents.close()
