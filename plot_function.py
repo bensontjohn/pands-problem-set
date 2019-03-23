31dfa7b5b3dfa930bb3f61379faedcb22a1ffd98
bensontjohn <bensontjohn@gmail.com>
2019-03-23 16:23:47 +0000

    Added references and comments for plot_function.py

--- plot_function.py
+++ plot_function.py
@@ -1,7 +1,19 @@
+# Benson Thomas John, 2019
+# Program that displays a plot of the functions x, x^2 and 2^x in the range [0,4].
+
+# Referenced: Matplotlib(https://matplotlib.org/users/pyplot_tutorial.html)
+
+# import matplotlib
 import matplotlib.pyplot as plt
-plt.plot([0,1,2,3])
-plt.plot([0, 1, 4, 9])
-plt.plot([1, 2, 4, 8])
+
+# plot of the functions x, x^2 and 2^x in the range [0,4]
+plt.plot([0, 1, 2, 3, 4])
+plt.plot([0, 1, 4, 9, 16])
+plt.plot([1, 2, 4, 8, 16])
+
+# Label for Y and X axis
 plt.ylabel('x, x^2 and 2^x ')
 plt.xlabel('x')
+
+# shows the plots
 plt.show()
