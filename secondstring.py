ad1aa722f74b3592aa1be25dea364ff354533e9b
bensontjohn <bensontjohn@gmail.com>
2019-03-29 17:39:53 +0000

    Added a space after lines 12 and 18

--- secondstring.py
+++ secondstring.py
@@ -10,11 +10,13 @@ user_input = input("Please enter a sentence: ")
 arr_input = user_input.split(' ')
 
 # Adapted: https://www.programiz.com/python-programming/methods/built-in/slice
+
 # obj[start:stop:step] - using indexing syntax
 odd_list = arr_input[0::2]
 
 # Convert the array to string using join method
 output_string = " ".join(odd_list)
+
 # Print the string that returns every second word.
 print(output_string)
 
